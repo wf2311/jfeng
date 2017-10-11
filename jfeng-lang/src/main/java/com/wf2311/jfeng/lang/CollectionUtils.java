6673df69ad4a5411bae7b111c92d247d58570375
wf2311 <wf2311@163.com>
2017-10-11 22:36:04 +0800

    list转换数组

--- jfeng-lang/src/main/java/com/wf2311/jfeng/lang/CollectionUtils.java
+++ jfeng-lang/src/main/java/com/wf2311/jfeng/lang/CollectionUtils.java
@@ -1,11 +1,15 @@
 package com.wf2311.jfeng.lang;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author wangfeng
@@ -42,6 +46,38 @@ public class CollectionUtils {
     public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    /**
+     * List类型转换
+     * <pre>
+     *     List<Integer> integerList = convertList(Arrays.asList("1","2","3"), s -> Integer.parseInt(s));
+     * </pre>
+     *
+     * @param source 源list
+     * @param func   转换函数
+     * @param <T>    源list类型
+     * @param <U>    目标list类型
+     */
+    public static <T, U> List<U> convertList(List<T> source, Function<T, U> func) {
+        return source.stream().map(func).collect(toList());
+    }
+
+    /**
+     * List转换为数组
+     * <pre>
+     *   Double[] doubleArr = convertArray(Arrays.asList("1","2","3"), Double::parseDouble, Double[]::new);
+     * </pre>
+     *
+     * @param source    源list
+     * @param func      转换函数
+     * @param generator 函数：产生一个数组
+     * @param <T>       源list类型
+     * @param <U>       目标list类型
+     * @return
+     */
+    public static <T, U> U[] convertArray(T[] source, Function<T, U> func, IntFunction<U[]> generator) {
+        return Arrays.stream(source).map(func).toArray(generator);
     }
 
 }
